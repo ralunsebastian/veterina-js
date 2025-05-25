7258e2f530012f1978fab9e361b75abbacee1ba6
Sebastian Oyarzo <oyarzo.sebastian@gmail.com>
2025-05-24 22:43:04 -0400

    primer commit de veterina js

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const { registrar, leer } = require("./operaciones");
+
+const [operacion, nombre, edad, tipo, color, enfermedad] = process.argv.slice(2);
+
+if (operacion === "registrar") {
+    registrar(nombre, edad, tipo, color, enfermedad);
+}
+
+if (operacion === "leer") {
+    leer();
+}
---
+++ operaciones.js
@@ -0,0 +1,27 @@
+// operaciones.js
+const fs = require("fs");
+
+const registrar = (nombre, edad, tipo, color, enfermedad) => {
+    const citas = JSON.parse(fs.readFileSync("citas.json", "utf8"));
+
+    const nuevaCita = {
+        nombre,
+        edad,
+        tipo,
+        color,
+        enfermedad
+    };
+
+    citas.push(nuevaCita);
+
+    fs.writeFileSync("citas.json", JSON.stringify(citas, null, 2));
+    console.log("✅ Cita registrada con éxito.");
+};
+
+const leer = () => {
+    const citas = JSON.parse(fs.readFileSync("citas.json", "utf8"));
+    console.log("📋 Citas registradas:");
+    console.log(citas);
+};
+
+module.exports = { registrar, leer };
